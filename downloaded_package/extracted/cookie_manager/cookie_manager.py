83458bd3fa510793f399bd2a874efffe1eb5e871
William Schmitt <william.schmitt@snowflake.com>
2024-09-09 11:48:25 -0700

    🥑

---
+++ downloaded_package/extracted/cookie_manager/cookie_manager.py
@@ -0,0 +1,219 @@
+import json
+import re
+
+from itsdangerous import TimestampSigner, BadSignature, SignatureExpired
+
+
+class CookieManager:
+    _config: dict = {
+        "VERIFY_MAX_COOKIE_AGE": 50,  # Cookie TTL in seconds (enforced when verifying, not when signing)
+    }
+    _logger = type(
+        "logger",
+        (),
+        {
+            "critical": lambda msg: print(msg),
+            "error": lambda msg: print(msg),
+            "warning": lambda msg: print(msg),
+            "debug": lambda msg: print(msg),
+            "info": lambda msg: print(msg),
+        },
+    )
+    _exceptions = type(
+        "exception",
+        (),
+        {
+            "Unauthorized": Exception,
+            "ServiceUnavailable": Exception,
+            "BadRequest": Exception,
+        },
+    )
+    _keys = {}  # Signing/verification keys in the format {"key_id": "key")
+    cookie_name = None
+
+    def __init__(
+        self,
+        keys: dict,
+        config: dict = None,
+        logger=None,
+        exceptions=None,
+    ) -> None:
+        if config:
+            self._config = self._override_config(override_config=config)
+        if logger:
+            self._logger = logger
+
+        if keys is None:
+            self._logger.critical(f"Signing/verification keys not supplied.")
+            raise self._exceptions.ServiceUnavailable
+
+        if exceptions:
+            self._exceptions = exceptions
+
+        self._keys = keys
+        self._logger.info(f"Finished configuring cookie manager")
+
+    def sign(self, cookie: dict, key_id: str) -> str:
+        """
+        Sign and encode a cookie ready for transport and secure comms with trusted services
+        Use a pre-registered signing key, looked up through ``key`` and ``self._keys``
+        Will add ``key_id`` to the cookie payload (used to verify)
+        :param cookie: Cookie dict to sign, e.g. {"A": "A"}
+        :param key_id: Id of signing key registered in ``self._keys``
+        :return: Signed cookie string, e.g. {"A": "A"}.ABCD.12345678
+        """
+        self._logger.info(f"Starting to sign cookie: {cookie} with key_id: {key_id}")
+        try:
+            signing_key = self._keys[key_id]
+        except KeyError:
+            self._logger.error(f"Bad lookup for signing key_id: {key_id}")
+            raise self._exceptions.ServiceUnavailable
+
+        cookie["key_id"] = key_id
+        signed_cookie = self._sign_cookie(cookie=cookie, signing_key=signing_key)
+        return signed_cookie
+
+    def verify(self, signed_cookie: str) -> dict:
+        """
+        Takes a signed cookie, extracts the ``key_id`` embedded in it, and verifies then decodes the cookie
+        :param signed_cookie: Signed cookie payload, containing a ``key_id`` that matches a key in ``self._keys``
+        :return: Unsigned, trusted cookie as an object
+        """
+        key_id = self._extract_key_id(signed_cookie=signed_cookie)
+        try:
+            key = self._keys[key_id]
+        except KeyError:
+            self._logger.error(f"Bad lookup for verifying key_id: {key_id}")
+            raise self._exceptions.ServiceUnavailable
+
+        return self._decode_verify_cookie(cookie=signed_cookie, verify_key=key)
+
+    def _override_config(self, override_config: dict) -> dict:
+        """
+        Takes a config dict ``override_config`` and overrides any default values found in ``self._config``
+        Will error out if a config element does not exist in the parent dict
+        :param override_config: Dict of config options
+        :return: copy of ``self._config`` dict with values updated by those supplied in ``override_config``
+        """
+        self._logger.info(f"Starting to compare config: {override_config}")
+        new_config: dict = self._config
+
+        if override_config:
+            self._logger.debug("Override config provided")
+            for key, value in override_config.items():
+                self._logger.debug(f"Comparing config entry: {key} - {value}")
+                try:
+                    # Only override existing values
+                    new_config[key]
+                except KeyError:
+                    self._logger.error(f"Refusing to override config: {key} - {value}")
+                    raise self._exceptions.BadRequest
+                new_config[key] = value
+
+        self._logger.info(f"Finished comparing config: {override_config}")
+        return new_config
+
+    def _decode_verify_cookie(self, cookie: str, verify_key: str) -> [None, dict]:
+        """
+        Verify and decode a signed cookie payload from other internal services.
+        Will trigger ``self.failure_func`` with a http status code upon error
+        Logs status to ``self.logger``
+        :param cookie: Signed payload for one cookie
+        :param verify_key: Str used to verify original signer of cookie
+        :return: Verified cookie dict payload, None,
+        """
+        self._logger.info(f"Starting to verify cookie: {cookie}")
+
+        if cookie is None:
+            self._logger.warning(f"Incoming cookie '{cookie}' not provided.")
+            raise self._exceptions.Unauthorized
+        try:
+            self._logger.debug(f"Beginning verification: {cookie}")
+            cookie_value = TimestampSigner(verify_key).unsign(
+                cookie, max_age=self._config.get("VERIFY_MAX_COOKIE_AGE", 50)
+            )
+        except BadSignature as e:
+            self._logger.warning(
+                f"Incoming cookie payload: '{cookie}' failed validation: {e}"
+            )
+            raise self._exceptions.Unauthorized
+        except SignatureExpired as e:
+            self._logger.warning(
+                f"Incoming cookie payload '{cookie}' no longer valid (too old/time mismatch): {e}"
+            )
+            raise self._exceptions.Unauthorized
+        except Exception as e:
+            self._logger.error(f"Incoming cookie object: '{cookie}' problem: {e}")
+            raise self._exceptions.ServiceUnavailable
+
+        self._logger.info(f"Finished verifying cookie: {cookie}")
+        self._logger.info(f"Beginning to json decode: {cookie_value}")
+        try:
+            cookie_payload = json.loads(cookie_value)
+        except ValueError:
+            self._logger.warning(
+                f"Could not decode incoming verified cookie: {cookie_value}"
+            )
+            cookie_payload = None
+
+        self._logger.info(f"Finished decoding cookie: {cookie_payload}")
+        return cookie_payload
+
+    def _sign_cookie(self, cookie: dict, signing_key: str) -> str:
+        """
+        Sign and encode a cookie ready for transport and secure comms with trusted services
+        Use ``self.sign_cookie`` for public use
+        :param cookie: dict of data to sign
+        :param signing_key: key to sign data with
+        :return: str signed cookie payload
+        """
+        self._logger.info(f"Starting to sign cookie: {cookie}")
+        encoded_payload = json.dumps(cookie)
+
+        try:
+            result: str = (
+                TimestampSigner(secret_key=signing_key)
+                .sign(encoded_payload)
+                .decode("utf8")
+            )
+        except Exception as e:
+            self._logger.error(
+                f"Unexpected problem signing cookie payload: {cookie}: {e}"
+            )
+            raise self._exceptions.ServiceUnavailable
+
+        self._logger.info(f"Finished signing cookie: {cookie}")
+        return result
+
+    def _extract_key_id(self, signed_cookie: str) -> [str, None]:
+        """
+        Extract the ``key_id`` from a signed and unverified cookie
+        This is used to choose a key to verify this cookie, so it's not trusted at this stage
+        :param signed_cookie: Signed cookie string
+        :return: ``key_id``, used later on to choose a key
+        """
+        self._logger.info(f"Starting to extract key_id from {signed_cookie}")
+        untrusted_payload_re = re.search(r"(.*\}+).*", signed_cookie)
+
+        try:
+            untrusted_payload = untrusted_payload_re.group(1)
+        except AttributeError:
+            self._logger.error(f"Untrusted cookie in wrong format: {signed_cookie}")
+            raise self._exceptions.Unauthorized
+
+        try:
+            untrusted_object = json.loads(untrusted_payload)
+        except ValueError:
+            self._logger.error(
+                f"Unable to decode untrusted cookie to json: {signed_cookie}"
+            )
+            raise self._exceptions.Unauthorized
+
+        try:
+            key_id = untrusted_object["key_id"]
+        except KeyError:
+            self._logger.warning(f"No key_id provided by cookie: {untrusted_payload}")
+            key_id = None
+
+        self._logger.info(f"Finished extracting key_id from {signed_cookie}")
+        return key_id
---
+++ downloaded_package/extracted/tests/test_cookie_manager.py
@@ -0,0 +1,193 @@
+import pytest
+import json
+import mock
+
+from freezegun import freeze_time
+from cookie_manager.cookie_manager import CookieManager
+from werkzeug import exceptions
+from itsdangerous import TimestampSigner
+
+
+class TestCookieManager:
+    cookie_manager = None
+
+    def setup_method(self):
+        self.cookie_manager = CookieManager(keys={"A": "A"}, exceptions=exceptions)
+
+    @freeze_time("2019-12-06")
+    def test_verify_positive(self):
+        cookie_value = json.dumps({"A": "B", "key_id": "A"})
+
+        signed_cookie = (
+            TimestampSigner(secret_key="A").sign(cookie_value).decode("utf8")
+        )
+
+        unsigned_cookie = self.cookie_manager.verify(signed_cookie=signed_cookie)
+
+        assert unsigned_cookie == json.loads(cookie_value)
+
+    @freeze_time("2019-12-06")
+    def test_decode_verify_positive(self):
+        cookie_value = json.dumps({"A": "B"})
+
+        signed_cookie = (
+            TimestampSigner(secret_key="test_key").sign(cookie_value).decode("utf8")
+        )
+
+        unsigned_cookie = self.cookie_manager._decode_verify_cookie(
+            cookie=signed_cookie, verify_key="test_key"
+        )
+
+        assert unsigned_cookie == json.loads(cookie_value)
+
+    @freeze_time("2019-12-06")
+    def test_decode_verify_empty_cookie(self):
+        with pytest.raises(exceptions.Unauthorized):
+            self.cookie_manager._decode_verify_cookie(
+                cookie="", verify_key="test_key",
+            )
+
+    @freeze_time("2019-12-06")
+    def test_decode_verify_bad_signature(self):
+        cookie_value = json.dumps({"A": "B",})
+
+        signed_cookie = (
+            TimestampSigner(secret_key="test_key").sign(cookie_value).decode("utf8")
+        )
+
+        with pytest.raises(exceptions.Unauthorized):
+            self.cookie_manager._decode_verify_cookie(
+                cookie=signed_cookie, verify_key="wrong_key"
+            )
+
+    @freeze_time("2019-12-06")
+    def test_decode_verify_expired_signature(self):
+        cookie_value = json.dumps({"A": "B"})
+
+        signed_cookie = (
+            TimestampSigner(secret_key="test_key").sign(cookie_value).decode("utf8")
+        )
+
+        with pytest.raises(exceptions.Unauthorized):
+            self.cookie_manager._decode_verify_cookie(
+                cookie=f"{signed_cookie}.XemaAA.3N_BtXRXlZr1JUA-p6rNUwaCFTY",
+                verify_key="test_key",
+            )
+
+    @freeze_time("2019-12-06")
+    @mock.patch("itsdangerous.TimestampSigner.unsign")
+    def test_decode_verify_general_error(self, mocked_timestamp_signer):
+        mocked_timestamp_signer.side_effect = KeyError  # Any error will do
+
+        cookie_value = json.dumps({"A": "B"})
+
+        signed_cookie = (
+            TimestampSigner(secret_key="test_key").sign(cookie_value).decode("utf8")
+        )
+
+        with pytest.raises(exceptions.HTTPException):
+            self.cookie_manager._decode_verify_cookie(
+                cookie=signed_cookie, verify_key="test_key",
+            )
+
+    @freeze_time("2019-12-06")
+    @mock.patch("json.loads")
+    def test_decode_verify_invalid_json(self, mocked_json_loads):
+        mocked_json_loads.side_effect = ValueError
+
+        cookie_value = json.dumps({"A": "B"})
+
+        signed_cookie = (
+            TimestampSigner(secret_key="test_key").sign(cookie_value).decode("utf8")
+        )
+
+        result = self.cookie_manager._decode_verify_cookie(
+            cookie=signed_cookie, verify_key="test_key",
+        )
+
+        assert result is None
+
+    @freeze_time("2019-12-06 14:22:00")
+    def test_sign_cookie_positive(self):
+        cookie_value = {"A": "B"}
+
+        signed_cookie = self.cookie_manager._sign_cookie(
+            cookie=cookie_value, signing_key="test_key"
+        )
+
+        assert (
+            signed_cookie
+            == f"{json.dumps(cookie_value)}.XepkCA.CUZtVTCXHbqoalWVCh5xOa4S4WE"
+        )
+
+    @freeze_time("2019-12-06")
+    @mock.patch("itsdangerous.TimestampSigner.sign")
+    def test_sign_cookie_signing_error(self, mocked_timestamp_signer_sign):
+        mocked_timestamp_signer_sign.side_effect = KeyError  # Any error will do
+        with pytest.raises(exceptions.ServiceUnavailable):
+            self.cookie_manager._sign_cookie(cookie={}, signing_key="test_key")
+
+    @pytest.mark.parametrize(
+        "input_,expected_output",
+        [
+            ({"A": "1", "B": "B", "C": "C"}, {"A": "1", "B": "B", "C": "C"}),
+            ({"A": "1"}, {"A": "1", "B": "B", "C": "C"}),
+            (None, {"A": "A", "B": "B", "C": "C"}),
+            ({}, {"A": "A", "B": "B", "C": "C"}),
+        ],
+    )
+    def test_override_config_positive(self, input_, expected_output):
+        self.cookie_manager._config = {"A": "A", "B": "B", "C": "C"}
+        result = self.cookie_manager._override_config(override_config=input_)
+        assert result == expected_output
+
+    def test_override_config_negative(self):
+        self.cookie_manager._config = {"A": "A", "B": "B", "C": "C"}
+        bad_override_config = {"D": "D"}
+
+        with pytest.raises(exceptions.BadRequest):
+            self.cookie_manager._override_config(override_config=bad_override_config)
+
+    @pytest.mark.parametrize(
+        "input_,expected_output",
+        [
+            (
+                '{"A": "B", "key_id": "test_key_id"}.XepkCA.CUZtVTCXHbqoalWVCh5xOa4S4WE',
+                "test_key_id",
+            ),
+            ('{"A": "B"}.XepkCA.CUZtVTCXHbqoalWVCh5xOa4S4WE', None),
+        ],
+    )
+    def test_extract_key_id_positive(self, input_, expected_output):
+        result = self.cookie_manager._extract_key_id(signed_cookie=input_)
+        assert result == expected_output
+
+    @pytest.mark.parametrize(
+        "input_,error",
+        [
+            ("RANDOM_BLAH", exceptions.Unauthorized),
+            ("RANDOM_BLAH}", exceptions.Unauthorized),
+        ],
+    )
+    def test_extract_key_id_negative(self, input_, error):
+        with pytest.raises(error):
+            self.cookie_manager._extract_key_id(signed_cookie=input_)
+
+    @freeze_time("2019-12-06 14:22:00")
+    def test_sign_positive(self):
+        cookie_value = {"A": "B"}
+        self.cookie_manager._keys = {"key_id": "test_key"}
+        signed_cookie = self.cookie_manager.sign(cookie=cookie_value, key_id="key_id")
+
+        assert (
+            signed_cookie
+            == f"{json.dumps(cookie_value)}.XepkCA.S6GGQPTUJw4MWDLvvyZhwcMZ1W8"
+        )
+
+    @freeze_time("2019-12-06 14:22:00")
+    def test_sign_negative(self):
+        cookie_value = {"A": "B"}
+
+        with pytest.raises(exceptions.ServiceUnavailable):
+            self.cookie_manager.keys = {"C": "D"}
+            self.cookie_manager.sign(cookie=cookie_value, key_id="bad_key")
